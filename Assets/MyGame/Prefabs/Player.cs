35592e4f8bf10c0153b12bcf0d3cb94cc9dc45d4
JoeH-GD <joe.haldon.gd@gmail.com>
2022-03-03 01:38:42 +0300

    Update Player.cs

--- Assets/MyGame/Prefabs/Player.cs
+++ Assets/MyGame/Prefabs/Player.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,7 +8,11 @@ public class Player : MonoBehaviour
 {
        [SerializeField] private float playerSpeed;
        [SerializeField] private Vector3 direction;
+       
+        //ссылка на компонент, который добавляет физику
        public Rigidbody rb;
+
+       //Поля для силы прыжка и силы гравитации (чтобы не было парения в воздухе)
        public float jumpAmount;
        public float gravityScale;
       
@@ -30,21 +34,28 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    { 
+            //получаем направление движения согласно вводу игрока
             direction.z = Input.GetAxis("Vertical");
             direction.x = Input.GetAxis("Horizontal");
+
+            //обрабатываем инпут
             if (Input.GetKeyDown(KeyCode.Space))
             {
+                //добавляем прыжок с вектором вверх, силой прыжка и импульсом
                 rb.AddForce(Vector2.up * jumpAmount, ForceMode.Impulse);
             }
         }
 
     void FixedUpdate()
         {
+            // получаем скорость движения при помощи направления, заданной скорости, распределенной по времени
             var speed = direction * playerSpeed * Time.deltaTime;
             transform.Translate(speed);
 
             jumpAmount = 10; 
+
+            //добавляем гравитации
             rb.AddForce(Physics.gravity * (gravityScale - 1) * rb.mass);
         }
     }
